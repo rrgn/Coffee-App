18514cf601cbaa1028bb83bed31fcd54e2cf174c
Regan <regs.co@gmail.com>
2016-07-06 16:17:09 -0400

    added functionality

--- index2.js
+++ index2.js
@@ -60,53 +60,81 @@ app.post('/login', function(request, response) {
   var data = request.body;
   var userPassword = data.password;
   User.findOne( { _id: data.username }, function(err, user) {
-    if (err) {
-      console.log("error in finding user", err);
+    if (!user) {
+      response.json({
+        status: 'fail',
+        message: 'Invalid username or password'
+      });
       return;
     }
-    console.log("found user: ", user);
+
     var encryptedPassword = user.encryptedPassword;
     bcrypt.compare(userPassword, encryptedPassword, function(err, matched) {
-      if (err) {
-        console.error(err.message);
-        return;
-      }
-      if (matched) {
-        var token = randtoken.generate(64);
-        response.json({
-          status: 'ok',
-          token: token
-        });
-        var tokenArray = user.authenticationTokens;
-        tokenArray.push(token);
-        console.log(tokenArray);
-        console.log('You are logged in!');
-        user.save(function(err) {
-          if (err) {
-            console.log('error in saving token: ', err);
-            response.status(409);
-            response.json({
-              status: "fail",
-              message: "Username is taken"
-            });
-            return;
-          }
-        });
-      } else {
+      if (err || !matched) {
         response.json({
           status: 'fail',
           message: 'Invalid username or password'
         });
-        console.log('Invalid password');
+        return;
       }
+      var token = randtoken.generate(64);
+      var tokenArray = user.authenticationTokens;
+      tokenArray.push(token);
+        user.save(function(err) {
+        if (err || !matched) {
+          response.json({
+            status: "fail",
+            message: "Invalid username or password"
+          });
+          return;
+        }
+        response.json({
+          status: 'ok',
+          token: token
+        });
+      });
     });
   });
 });
 
 app.post('/orders', function(request, response) {
-  var data = response.body;
-  console.log(data);
-  response.send(data);
+  var result = request.body;
+  var token = result.token;
+  User.findOne({ authenticationTokens: token }, function(err, user) {
+    if(!user) {
+      response.json({
+        status: "fail",
+        message: "user not logged in"
+      });
+      return;
+    }
+    user.orders.push(result.order);
+    user.save(function(err) {
+      if (err) {
+        response.json({
+          status: "failed",
+          message: "Order failed" + err.message + JSON.stringify(err.errors)
+        });
+        return;
+      }
+      response.send('Place Order');
+    });
+  });
+});
+
+app.get('/orders', function(request, response) {
+  var result = request.body;
+  var token = request.query.token;
+  User.findOne({ authenticationTokens: token }, function(err, user) {
+    if(!user) {
+      response.json({
+        status: "fail",
+        message: "user not logged in"
+      });
+      return;
+    }
+  });
+  response.send('ok');
 });
 
 app.listen(3000, function() {
